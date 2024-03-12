94a1cee9f490b28d3a428b0124671d881bda3df7
Álvaro Sebastián Acosta Cortizas <145587906+alvarosac99@users.noreply.github.com>
2024-03-12 15:13:49 +0100

    :(

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
+window.onload = function() {
+    var idiomaAlmacenado = localStorage.getItem('idioma');
+    if (idiomaAlmacenado) {
+        aplicarTraducciones(idiomaAlmacenado);
+        document.getElementById('selector_idioma').value = idiomaAlmacenado;
+    }
+};
+
 document.getElementById('selector_idioma').addEventListener('change', function () {
     var idiomaSeleccionado = this.value;
-    fetch('../js/main.json')
+    localStorage.setItem('idioma', idiomaSeleccionado);
+    aplicarTraducciones(idiomaSeleccionado);
+});
+
+function aplicarTraducciones(idioma) {
+    fetch('main.json')
         .then(response => response.json())
         .then(traducciones => {
-            document.getElementById('titulo').innerHTML = traducciones[idiomaSeleccionado].titulo;
-            document.getElementById('tituloPiedraPapel').innerHTML = traducciones[idiomaSeleccionado].tituloPiedraPapel;
+            document.getElementById('titulo').innerHTML = traducciones[idioma].titulo;
         })
         .catch(error => console.error('Error al cargar el archivo JSON:', error));
-});
+}
